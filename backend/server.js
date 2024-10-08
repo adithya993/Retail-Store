d43889124fb6196e9aea89f99a4a66630bc16ea7
adithya993 <adithya.rengarajan993@gmail.com>
2024-10-07 19:40:33 -0700

    pushing prod ready version

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,15 @@ import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
 
+import path from "path";
+
 dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 app.use(express.json()); //allows us to accept json data in the req.body
 
@@ -16,6 +20,14 @@ app.get("/", (req, res) => {
 })
 
 app.use("/api/products", productRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"/frontend/dist")));
+
+    app.get("*",(req, res) => {
+        res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
+    })
+}
 
 app.listen(PORT, () => {
     connectDB();
